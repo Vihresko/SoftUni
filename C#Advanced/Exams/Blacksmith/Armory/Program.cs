2653535f66e5a4dc36a106721dd1ca8a84dd941b
Vihresko <vihrenstefanov@gmail.com>
2022-01-11 22:25:35 +0200

    add exams

---
+++ C#Advanced/Exams/Blacksmith/Armory/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Armory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            int[] officerPosition = new int[2];
+            List<int[]> mirrorsPositions = new List<int[]>();
+            FullfillMatrix(n, matrix, officerPosition, mirrorsPositions);
+            PrintMatrix(matrix);
+
+            int target = 65;
+
+
+        }
+
+        static void FullfillMatrix(int n, char[,] matrix, int[] officerPosition, List<int[]> mirrorsPositions)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                char[] inputValues = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] == 'A')
+                    {
+                        officerPosition[0] = i;
+                        officerPosition[1] = j;
+                    }
+
+                    if (matrix[i, j] == 'M')
+                    {
+                        mirrorsPositions.Add(new int[] { i, j });
+                    }
+
+                    matrix[i, j] = inputValues[j];
+                }
+            }
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static Dictionary<char, int[]> GetElementOnNextStep(string moveTo,int[] currentPostion, char[,] matrix)
+        {
+            int[] positionForCheck = currentPostion;
+            if(moveTo == "left")
+            {
+                positionForCheck[0] -= 1;
+            }
+            else if(moveTo == "right")
+            {
+                positionForCheck[0] += 1;
+            }
+            else if(moveTo == "up")
+            {
+                positionForCheck[1] -= 1;
+            }
+            else if(moveTo == "down")
+            {
+                positionForCheck[1] += 1;
+            }
+
+            return new Dictionary<char, int[]> { { matrix[positionForCheck[0], positionForCheck[1]], positionForCheck } };
+        }
+
+        static bool isNextPositionInRange(int matrixSize , int[] coordinates)
+        {
+            if(coordinates[0] < 0 || coordinates[0] >= matrixSize || coordinates[1] < 0 || coordinates[1] >= matrixSize)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ C#Advanced/Exams/Blacksmith/Blacksmith/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace Blacksmith
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> forgedWeapon = new Dictionary<string, int>();
+            var steels = new Queue<int>();
+            var carbons = new Stack<int>();
+
+            Dictionary<int, Dictionary<string, int>> swords = GetTargetedSwords();
+            CollectResources(steels,carbons);
+
+            //StartCraft
+            while (steels.Count > 0 && carbons.Count > 0)
+            {
+                int steel = steels.Peek();
+                int carbon = carbons.Peek();
+                forgedWeapon = swords.FirstOrDefault(s => s.Key == steel + carbon).Value;
+               if(forgedWeapon == null)
+                {
+                    steels.Dequeue();
+                    carbons.Push(carbons.Pop() + 5);
+                }
+                else
+                {
+                    steels.Dequeue();
+                    carbons.Pop();
+                    forgedWeapon[forgedWeapon.Keys.First()] += 1;
+                }
+            }
+
+            //EndCraft
+            PrintHowMuchSwordsAreForged(swords);
+            PrintResourceStatus(steels, "Steel");
+            PrintResourceStatus(carbons, "Carbon");
+            PrintForgedSwords(swords);
+        }
+
+        private static void PrintHowMuchSwordsAreForged(Dictionary<int, Dictionary<string, int>> notes)
+        {
+            int result = 0;
+            foreach (var item in notes)
+            {
+                result += item.Value.Values.First();
+            }
+            if (result > 0)
+            {
+                Console.WriteLine($"You have forged {result} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+        }
+
+        private static void CollectResources(Queue<int> steels, Stack<int> carbons)
+        {
+            int[] steelInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            foreach (var steel in steelInput)
+            {
+                steels.Enqueue(steel);
+            }
+
+            int[] carbonInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            foreach (var carbon in carbonInput)
+            {
+                carbons.Push(carbon);
+            }
+        }
+
+        private static Dictionary<int, Dictionary<string, int>> GetTargetedSwords()
+        {
+            Dictionary<int, Dictionary<string, int>> result = new Dictionary<int, Dictionary<string, int>>()
+            {
+                {70, new Dictionary<string, int>() {{"Gladius",0 }} },
+                {80, new Dictionary<string, int>() { {"Shamshir",0 }} },
+                {90, new Dictionary<string, int>() {{"Katana", 0 }} },
+                {110, new Dictionary<string, int>()  {{"Sabre", 0 }} },
+                {150,new Dictionary<string, int>() {{"Broadsword",0 }} } 
+            };
+            return result;
+        }
+        
+        private static void PrintResourceStatus<T>(IEnumerable<T> resources, string resourceName)
+        {
+            if(resources.Count() <= 0)
+            {
+                Console.WriteLine($"{resourceName} left: none");
+            }
+            else
+            {
+                Console.Write($"{resourceName} left: ");
+                Console.WriteLine(String.Join(", ", resources));
+            }
+           
+        }
+
+        private static void PrintForgedSwords(Dictionary<int, Dictionary<string, int>> swords)
+        {
+            foreach (var sword in swords.OrderBy(s=> s.Value.Keys.First()))
+            {
+                if(sword.Value.Any(s=> s.Value > 0))
+                {
+                    string swordName = sword.Value.Keys.First();
+                    int countForged = sword.Value.Values.First();
+                    Console.WriteLine($"{swordName}: {countForged}");
+                }
+            }
+        }
+    }
+}
